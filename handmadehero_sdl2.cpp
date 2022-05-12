d54498adf8a3f7fb6599b71a997267d700d3a6d1
Willy Clarke <willyclarke@mac.com>
2022-05-12 21:12:31 +0200

    BUGFIX: Start using my own handmadehero_sdl2 file
    WHY: Create a main platform file to follow along the HH series.
    HOW: Follow HH episode 2 and David Gow's tutorial.

---
+++ handmadehero_sdl2.cpp
@@ -0,0 +1,15 @@
+/******************************************************************************
+* * Filename : handmadehero_sdl2.cpp
+* * Date     : 2022 May 12
+* * Author   : Willy Clarke (willy@clarke.no)
+* * Version  : Use git you GIT
+* * Copyright: W. Clarke
+* * License  : MIT
+* * Descripti: SDL2 version of the Handmade Hero platform layer. Loosely
+*              following David Gows tutorial. See https://davidgow.net/handmadepenguin
+* ******************************************************************************/
+#include <SDL.h>
+
+auto main() -> int {
+  return 0;
+}
