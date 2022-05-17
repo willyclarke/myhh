d894ca9a63a6b126f54a83a6c6c612b0a37bc623
Willy Clarke <willyclarke@mac.com>
2022-05-17 19:56:21 +0200

    ENHANCEMENT: Add RenderWeirdGradient
    WHY: Test that it is possible to render RGB bitmaps.
    HOW: Create a semi platform independent RenderWeirdGradient function.

--- handmadehero_sdl2.cpp
+++ handmadehero_sdl2.cpp
@@ -23,12 +23,13 @@
 #define local_persist static
 #define global_variable static
 
+#ifndef HANDMADE
+#define HANDMADE 0
+#endif
 namespace
 {
 
 struct window_data {
-  int ScreenWidth{};
-  int ScreenHeight{};
   int BitmapWidth{};
   int BitmapHeight{};
   int BytesPerPixel{4};
@@ -60,43 +61,43 @@ internal void SDLResizeTexture(window_data *ptrWindowData,  //!<
                                int Width,                   //!<
                                int Height)
 {
-  if (ptrWindowData->Texture) {
-    SDL_DestroyTexture(ptrWindowData->Texture);
+  if (ptrWindowData->ptrTexture) {
+    SDL_DestroyTexture(ptrWindowData->ptrTexture);
   }
 
-  ptrWindowData->Texture = SDL_CreateTexture(Renderer,                     //!<
-                                             SDL_PIXELFORMAT_ARGB8888,     //!<
-                                             SDL_TEXTUREACCESS_STREAMING,  //!<
-                                             Width,                        //!<
-                                             Height                        //!<
+  ptrWindowData->ptrTexture = SDL_CreateTexture(Renderer,                     //!<
+                                                SDL_PIXELFORMAT_ARGB8888,     //!<
+                                                SDL_TEXTUREACCESS_STREAMING,  //!<
+                                                Width,                        //!<
+                                                Height                        //!<
   );
 
-  void *BitmapMemory = mmap(nullptr,                                         //!< Pointer to start
+  void *BitmapMemory = mmap(nullptr,                                        //!< Pointer to start
                             Width * Height * ptrWindowData->BytesPerPixel,  //!<
-                            PROT_READ | PROT_WRITE,                          //!<  Protection
-                            MAP_ANONYMOUS | MAP_PRIVATE,                     //!<  Flags
+                            PROT_READ | PROT_WRITE,                         //!<  Protection
+                            MAP_ANONYMOUS | MAP_PRIVATE,                    //!<  Flags
                             -1,  //!<  File descriptor, -1 when we dont want to map a file.
                             0    //!<  Offset
   );
 
-  if (ptrWindowData->BitmapMemory) {
-    munmap(ptrWindowData->BitmapMemory,       //!<
+  if (ptrWindowData->ptrBitmapMemory) {
+    munmap(ptrWindowData->ptrBitmapMemory,    //!<
            ptrWindowData->BitmapWidth *       //!<
                ptrWindowData->BitmapHeight *  //!<
-               ptrWindowData->BytesPerPixel  //!<
+               ptrWindowData->BytesPerPixel   //!<
     );
   }
 
   ptrWindowData->BitmapWidth = Width;
   ptrWindowData->BitmapHeight = Height;
-  ptrWindowData->BitmapMemory = BitmapMemory;
+  ptrWindowData->ptrBitmapMemory = BitmapMemory;
 
-  ptrWindowData->BitmapUpdated = ptrWindowData->BitmapMemory != nullptr;
+  ptrWindowData->BitmapUpdated = ptrWindowData->ptrBitmapMemory != nullptr;
 }
 
 //------------------------------------------------------------------------------
 internal void SDLUpdateWindow(window_data *ptrWindowData,  //!<
-                              SDL_Renderer *Renderer       //!<
+                              SDL_Renderer *ptrRenderer    //!<
                                                            //
 )
 {
@@ -109,9 +110,9 @@ internal void SDLUpdateWindow(window_data *ptrWindowData,  //!<
   assert(ptrWindowData->Pixels);
   assert(ptrWindowData->TextureWidth);
 
-  if (SDL_UpdateTexture(ptrWindowData->Texture,                                      //!<
-                        0,                                                           //!<
-                        ptrWindowData->BitmapMemory,                                 //!<
+  if (SDL_UpdateTexture(ptrWindowData->ptrTexture,                                  //!<
+                        0,                                                          //!<
+                        ptrWindowData->ptrBitmapMemory,                             //!<
                         ptrWindowData->BitmapWidth * ptrWindowData->BytesPerPixel)  //!<
   ) {
     char ErrStr[256]{0};
@@ -120,10 +121,10 @@ internal void SDLUpdateWindow(window_data *ptrWindowData,  //!<
     return;
   }
 
-  if (SDL_RenderCopy(Renderer,                //!<
-                     ptrWindowData->Texture,  //!<
-                     nullptr,                 //!<
-                     nullptr)                 //!<
+  if (SDL_RenderCopy(ptrRenderer,                //!<
+                     ptrWindowData->ptrTexture,  //!<
+                     nullptr,                    //!<
+                     nullptr)                    //!<
   ) {
     char ErrStr[256]{0};
     printf("%s -> Error when doing RenderCopy. %s.\n", __FUNCTION__,
@@ -131,14 +132,21 @@ internal void SDLUpdateWindow(window_data *ptrWindowData,  //!<
     return;
   }
 
-  SDL_RenderPresent(Renderer);
+  SDL_RenderPresent(ptrRenderer);
 }
 
 /**
  * Event handler.
  */
-bool HandleEvent(SDL_Event *Event, window_data *ptrWindowData)
+internal bool HandleEvent(SDL_Event *Event, window_data *ptrWindowData)
 {
+#if HANDMADE
+  static int Count{};
+  ++Count;
+  std::cout << __FUNCTION__ << " -> " << Event->type << ". Count: " << Count
+            << ". Event type: " << Event->type << std::endl;
+#endif
+
   bool ShouldQuit = false;
   switch (Event->type) {
     case SDL_QUIT: {
@@ -147,14 +155,13 @@ bool HandleEvent(SDL_Event *Event, window_data *ptrWindowData)
     } break;
     case SDL_WINDOWEVENT: {
       switch (Event->window.event) {
-          // case SDL_WINDOWEVENT_RESIZED: {
         case SDL_WINDOWEVENT_SIZE_CHANGED: {
           printf("SDL_WINDOWEVENT_SIZE_CHANGED (%d, %d)\n",  //!<
                  Event->window.data1, Event->window.data2);
 
-          SDL_Window *Window = SDL_GetWindowFromID(Event->window.windowID);
+          SDL_Window *ptrWindow = SDL_GetWindowFromID(Event->window.windowID);
 
-          if (!Window) {
+          if (!ptrWindow) {
             printf(
                 "SDL_WINDOWEVENT_RESIZED (%d, %d) -> Failed to get pointer to "
                 "Window.\n",
@@ -163,30 +170,12 @@ bool HandleEvent(SDL_Event *Event, window_data *ptrWindowData)
             return ShouldQuit;
           }
 
-          SDL_Renderer *Renderer = SDL_GetRenderer(Window);
-
-          int Width{}, Height{};
-          SDL_GetWindowSize(Window, &Width, &Height);
-          ptrWindowData->ScreenHeight = Height;
-          ptrWindowData->ScreenWidth = Width;
-
-          SDLResizeTexture(ptrWindowData, Renderer, Event->window.data1, Event->window.data2);
-
+          SDL_Renderer *ptrRenderer = SDL_GetRenderer(ptrWindow);
+          SDLResizeTexture(ptrWindowData, ptrRenderer, Event->window.data1, Event->window.data2);
         } break;
         case SDL_WINDOWEVENT_EXPOSED: {
-          printf("SDL_WINDOWEVENT_EXPOSED (%d, %d)\n",  //!<
+          printf("SDL_WINDOWEVENT_EXPOSED (w:%d, h:%d)\n",  //!<
                  Event->window.data1, Event->window.data2);
-
-          SDL_Window *Window = SDL_GetWindowFromID(Event->window.windowID);
-          SDL_Renderer *Renderer = SDL_GetRenderer(Window);
-
-          // ---
-          // FIXME: (Willy Clarke) Need to figure out why texture is not available
-          //                       to the Render in the SDLUpdateWindow call below.
-          // ---
-          if (!ptrWindowData->BitmapUpdated) return (ShouldQuit);
-
-          SDLUpdateWindow(ptrWindowData, Renderer);
         } break;
       }
     } break;
@@ -194,6 +183,36 @@ bool HandleEvent(SDL_Event *Event, window_data *ptrWindowData)
   return (ShouldQuit);
 }
 };  // end of anonymous namespace
+
+/**
+Test code for SDLUpdateWindow(). The code in this function is kind of platform
+independent. But since it uses the window_data struct which contain both
+POD variables and SDL variables it is not strictly true.
+*/
+internal void RenderWeirdGradient(window_data *ptrWindowData,  //!<
+                                  SDL_Renderer *ptrRenderer,   //!<
+                                  int const BlueOffset = 0,    //!<
+                                  int const GreenOffset = 0    //!<
+)
+{
+  // int const Pitch = ptrWindowData->BitmapWidth * ptrWindowData->BytesPerPixel;
+
+  uint8_t *ptrBitmapMemory = (uint8_t *)ptrWindowData->ptrBitmapMemory;
+  uint32_t *Pixel = (uint32_t *)ptrBitmapMemory;
+
+  for (size_t Row = 0;                     ///<!
+       Row < ptrWindowData->BitmapHeight;  ///<!
+       ++Row) {
+    for (size_t Col = 0;                    ///<!
+         Col < ptrWindowData->BitmapWidth;  ///<!
+         ++Col) {
+      uint8_t const Blue = Row + BlueOffset;
+      uint8_t const Green = Col + GreenOffset;
+      *Pixel = (Green << 8) | Blue;
+      ++Pixel;
+    }
+  }
+}
 
 /**
  */
@@ -204,23 +223,43 @@ auto main() -> int
     return -1;
   }
 
-  SDL_Window *Window = SDL_CreateWindow("Handmade Hero", SDL_WINDOWPOS_UNDEFINED,
-                                        SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_RESIZABLE);
+  SDL_Window *ptrWindow = SDL_CreateWindow("Handmade Hero", SDL_WINDOWPOS_UNDEFINED,
+                                           SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_RESIZABLE);
 
   // Create a "Renderer" for our window.
-  SDL_Renderer *Renderer = SDL_CreateRenderer(Window,  //!<
-                                              -1,      //!< Auto detect.
-                                              0        //!< Render flags (if any).
+  SDL_Renderer *ptrRenderer = SDL_CreateRenderer(ptrWindow,  //!<
+                                                 -1,         //!< Auto detect.
+                                                 0           //!< Render flags (if any).
   );
 
   window_data WindowData{};
 
-  for (;;) {
+  int Width{}, Height{};
+  SDL_GetWindowSize(ptrWindow, &Width, &Height);
+  SDLResizeTexture(&WindowData, ptrRenderer, Width, Height);
+
+  bool Running{true};
+  int BlueOffset{};
+  int GreenOffset{};
+  int ScanCount{};
+
+  while (Running) {
     SDL_Event Event{};
-    SDL_WaitEvent(&Event);
-    if (HandleEvent(&Event, &WindowData)) {
-      break;
+    while (SDL_PollEvent(&Event)) {
+      if (HandleEvent(&Event, &WindowData)) {
+        Running = false;
+      }
     }
+    RenderWeirdGradient(&WindowData, ptrRenderer, BlueOffset, GreenOffset);
+
+    if (!(ScanCount % 10)) {
+      ScanCount = 0;
+      ++BlueOffset;
+      ++GreenOffset;
+    }
+    ++ScanCount;
+
+    SDLUpdateWindow(&WindowData, ptrRenderer);
   }
 
   SDL_Quit();
